4da42c4ec8821620263cd14c4be753823f83153d
Jacques Ikot <jacquesikot@gmail.com>
2023-09-27 09:36:06 +0100

    add support for UMD build pattern

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// index.js
-const myFunctions = require('./src/functions');
-
-module.exports = myFunctions;
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD module
+    define(['./src/functions'], factory);
+  } else if (typeof exports === 'object' && typeof module !== 'undefined') {
+    // CommonJS module (Node.js)
+    module.exports = factory(require('./src/functions'));
+  } else {
+    // Global variable (browser)
+    root.myFunctions = factory(root.myFunctions || {});
+  }
+})(typeof self !== 'undefined' ? self : this, function (myFunctions) {
+  // The UMD wrapper ensures that 'myFunctions' is available in the current environment.
+  return myFunctions;
+});
