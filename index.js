872c58f151eba510649938cf2c9ff4ea1c8c8205
Jacques Ikot <jacquesikot@gmail.com>
2023-09-27 10:04:34 +0100

    use webpack to bundle and minify code + remove UMD wrapper in index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD module
-    define(['./src/functions'], factory);
-  } else if (typeof exports === 'object' && typeof module !== 'undefined') {
-    // CommonJS module (Node.js)
-    module.exports = factory(require('./src/functions'));
-  } else {
-    // Global variable (browser)
-    root.myFunctions = factory(root.myFunctions || {});
-  }
-})(typeof self !== 'undefined' ? self : this, function (myFunctions) {
-  // The UMD wrapper ensures that 'myFunctions' is available in the current environment.
-  return myFunctions;
-});
+const myFunctions = require('./src/functions');
+
+module.exports = myFunctions;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './index.js', // Entry point of your module
+  output: {
+    filename: 'index.min.js', // Output file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    library: 'js-utility-library', // UMD export variable name
+    libraryTarget: 'umd', // UMD format
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()], // Minify the output
+  },
+};
